470e2db1fab61d2d0950a647205d3f5fbeda5dc4
Luke <luckyskywalker2003@gmail.com>
2024-08-19 12:13:11 +0200

    Added 3D rendering with a cube

--- Minecraft/main.cpp
+++ Minecraft/main.cpp
@@ -1,53 +1,221 @@
 #include <Windows.h>
-#include <gl/GL.h>
+#include <iostream>
+#include "glad/glad.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Vertex and fragment shader source code
+const char* vertexShaderSource = R"(
+    #version 330 core
+    layout(location = 0) in vec3 aPos;
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 projection;
+    void main()
+    {
+        gl_Position = projection * view * model * vec4(aPos, 1.0);
+    }
+)";
+
+const char* fragmentShaderSource = R"(
+    #version 330 core
+    out vec4 FragColor;
+    void main()
+    {
+        FragColor = vec4(1.0, 1.0, 1.0, 1.0); // White color
+    }
+)";
+
+GLfloat vertices[] = {
+    // Back face
+    -1.0f, -1.0f, -1.0f,  // V0
+     1.0f, -1.0f, -1.0f,  // V1
+     1.0f,  1.0f, -1.0f,  // V2
+    -1.0f,  1.0f, -1.0f,  // V3
+
+    // Front face
+    -1.0f, -1.0f,  1.0f,  // V4
+     1.0f, -1.0f,  1.0f,  // V5
+     1.0f,  1.0f,  1.0f,  // V6
+    -1.0f,  1.0f,  1.0f   // V7
+};
+
+GLuint indices[]{
+    // Back face
+    0, 1, 2,
+    2, 3, 0,
+
+    // Front face
+    4, 5, 6,
+    6, 7, 4,
+
+    // Left face
+    0, 4, 7,
+    7, 3, 0,
+
+    // Right face
+    1, 5, 6,
+    6, 2, 1,
+
+    // Top face
+    3, 2, 6,
+    6, 7, 3,
+
+    // Bottom face
+    0, 1, 5,
+    5, 4, 0
+};
 
 LRESULT CALLBACK WinProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	switch (uMsg)
-	{
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hwnd, uMsg, wParam, lParam);
-	}
-	return 0;
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProcW(hwnd, uMsg, wParam, lParam);
+    }
+    return 0;
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+GLuint compileShader(GLenum type, const char* source)
 {
-	// Create a window
-	WNDCLASS wc = {};
-	wc.lpfnWndProc = WinProc;
-	wc.hInstance = hInstance;
-	wc.lpszClassName = L"Minecraft";
-	RegisterClass(&wc);
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
 
-	HWND hwnd = CreateWindow(wc.lpszClassName, L"Minecraft", WS_OVERLAPPEDWINDOW, 0, 0, 800, 600, 0, 0, hInstance, 0);
+    GLint success;
+    GLchar infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+		std::cout << "Shader compilation error: " << infoLog << std::endl;
+    }
 
-	// Create an OpenGL context
-	HDC hdc = GetDC(hwnd);
-	PIXELFORMATDESCRIPTOR pfd = { sizeof(PIXELFORMATDESCRIPTOR), 1, PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER, PFD_TYPE_RGBA, 32 };
-	SetPixelFormat(hdc, ChoosePixelFormat(hdc, &pfd), &pfd);
-	wglMakeCurrent(hdc, wglCreateContext(hdc));
-	ShowWindow(hwnd, nCmdShow);
+    return shader;
+}
 
-	// Main loop
-	while (true)
-	{
-		MSG msg;
-		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
-		{
-			if (msg.message == WM_QUIT)
-				break;
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
+GLuint createShaderProgram()
+{
+    GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertexShaderSource);
+    GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
 
-		glClearColor(0.2f, 0.4f, 0.8f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		SwapBuffers(hdc);
-	}
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    GLint success;
+    GLchar infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+		std::cout << "Shader program linking error: " << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    // Create a window
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WinProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"Minecraft";
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindow(wc.lpszClassName, L"Minecraft", WS_OVERLAPPEDWINDOW, 0, 0, 800, 600, 0, 0, hInstance, 0);
+
+    // Create an OpenGL context
+    HDC hdc = GetDC(hwnd);
+    PIXELFORMATDESCRIPTOR pfd = { sizeof(PIXELFORMATDESCRIPTOR), 1, PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER, PFD_TYPE_RGBA, 32 };
+    SetPixelFormat(hdc, ChoosePixelFormat(hdc, &pfd), &pfd);
+    HGLRC hglrc = wglCreateContext(hdc);
+    wglMakeCurrent(hdc, hglrc);
+
+    // Initialize OpenGL
+	gladLoadGL();
+
+    // Create buffers
+    GLuint vbo, vao, ebo;
+    glGenBuffers(1, &vbo);
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &ebo);
+
+    // Bind buffers
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+
+    // Copy data to buffers
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Set vertex attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    GLuint shaderProgram = createShaderProgram();
+    glUseProgram(shaderProgram);
+
+    GLint modelLoc = glGetUniformLocation(shaderProgram, "model");
+    GLint viewLoc = glGetUniformLocation(shaderProgram, "view");
+    GLint projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+
+    // Projection matrix
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+    glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+    // View matrix
+    glm::mat4 view = glm::lookAt(glm::vec3(3.0f, 3.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+    // Model matrix
+    glm::mat4 model = glm::mat4(1.0f);
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+    ShowWindow(hwnd, nCmdShow);
+
+    // Main loop
+    while (true)
+    {
+        MSG msg;
+        if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+                break;
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        glClearColor(0.2f, 0.4f, 0.8f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+        SwapBuffers(hdc);
+    }
+
+    // Cleanup
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
+    glDeleteProgram(shaderProgram);
+
+    wglMakeCurrent(nullptr, nullptr);
+	wglDeleteContext(hglrc);
+	ReleaseDC(hwnd, hdc);
+	DestroyWindow(hwnd);
 
 	return 0;
 }
